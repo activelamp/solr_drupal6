4847715fdff3a704516c48f38c9ce7c9497e081f
Will <will@causecast.org>
2013-05-16 15:23:50 -0700

    Initial commit

---
+++ puppet/manifests/base.pp
@@ -0,0 +1,135 @@
+class { "solr":
+        source_url => "http://apache.mirrors.lucidnetworks.net/lucene/solr/3.6.2/apache-solr-3.6.2.tgz",
+        home_dir => "/usr/share/solr",
+        package => "apache-solr-3.6.2",
+        solr_data_dir => "/var/lib/solr/data",
+        cores => ["cor", "ug_data"],
+        tomcat_connector_port => "8983",
+    }
+
+  #Copy the respective schema.xml file
+  file { "/usr/share/solr/ug_data/conf/schema.xml":
+    ensure => present,
+    source  => "/vagrant/ug_data/schema.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  #Copy the respective solrconfig.xml file
+  file { "/usr/share/solr/ug_data/conf/solrconfig.xml":
+    ensure => present,
+    source  => "/vagrant/ug_data/solrconfig.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/ug_data/conf/stopwords.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/stopwords.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/ug_data/conf/synonyms.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/synonyms.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/ug_data/conf/mapping-ISOLatin1Accent.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/mapping-ISOLatin1Accent.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/ug_data/conf/protwords.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/protwords.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/ug_data/conf/elevate.xml":
+    ensure => present,
+    source  => "/vagrant/ug_data/elevate.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+#COR
+
+  #Copy the respective schema.xml file
+  file { "/usr/share/solr/cor/conf/schema.xml":
+    ensure => present,
+    source  => "/vagrant/cor/schema.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  #Copy the respective solrconfig.xml file
+  file { "/usr/share/solr/cor/conf/solrconfig.xml":
+    ensure => present,
+    source  => "/vagrant/cor/solrconfig.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/cor/conf/stopwords.txt":
+    ensure => present,
+    source  => "/vagrant/cor/stopwords.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/cor/conf/synonyms.txt":
+    ensure => present,
+    source  => "/vagrant/cor/synonyms.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+  
+  file { "/usr/share/solr/cor/conf/spellings.txt":
+    ensure => present,
+    source  => "/vagrant/cor/spellings.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+    file { "/usr/share/solr/cor/conf/mapping-ISOLatin1Accent.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/mapping-ISOLatin1Accent.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+  file { "/usr/share/solr/cor/conf/protwords.txt":
+    ensure => present,
+    source  => "/vagrant/ug_data/protwords.txt",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
+   file { "/usr/share/solr/cor/conf/elevate.xml":
+    ensure => present,
+    source  => "/vagrant/ug_data/elevate.xml",
+    group   => "tomcat6",
+    owner   => "tomcat6",
+    notify  => Service["tomcat6"],
+  }
+
